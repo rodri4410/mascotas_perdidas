542f8ac999c9b4201f2db176c4bf7263fa546f40
John Doe <rodrigoamartinez2002@gmail.com>
2023-10-16 18:40:33 -0300

    v2.1

--- JAVASCRIPTS/evento.js
+++ JAVASCRIPTS/evento.js
@@ -1,21 +1,23 @@
 function menus(){
     var opc= document.getElementById("OPC");
     opc.innerHTML= ` 
-    <div id="MENU3>         
-    <a href="adopta.html"> <button><img src="Archivos/adopcion.png" width="50px" height="50px">ADOPTAR
-                    </button> </a> 
-             <a href="formulario.html"> <button><img src="Archivos/desaparecido.png" width="50px"
-                            height="50px">PUBLICAR </button> </a> 
-             <a href="perdidos.html"><button><img src="Archivos/desaparecido.png" width="50px" height="50px"> VER
-                    </button> </a> 
-        <div>
+    <table id="MENU3">
+        <td> 
+            <tr> <a href="adopta.html"> <button><img src="Archivos/adopcion.png" width="50px" height="50px">ADOPTAR
+                    </button> </a> </tr>
+            <tr> <a href="formulario.html"> <button><img src="Archivos/desaparecido.png" width="50px"
+                            height="50px">PUBLICAR </button> </a> </tr>
+            <tr> <a href="perdidos.html"><button><img src="Archivos/desaparecido.png" width="50px" height="50px"> VER
+                    </button> </a> </tr>
+        </td>
 
+    </table>
     `
 }
 
 function menus2(){
     var opc= document.getElementById("OPC");
-    opc.innerHTML= ` <p id="OPC" onmouseenter="menus()" onmouseout="menus2()"> <img src="Archivos/opcion-de-lista.png" width="30px" height="30px"> </p>
+    opc.innerHTML= ` <p id="OPC" onclick="menus()"> <img src="Archivos/opcion-de-lista.png" width="30px" height="30px"> </p>
     `
 }
 
