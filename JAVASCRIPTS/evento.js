07bacc95d9e0aa70ae29bee9ba1874e80575b6d9
John Doe <rodrigoamartinez2002@gmail.com>
2023-10-16 17:38:34 -0300

    Con menu desplegable PC

--- JAVASCRIPTS/evento.js
+++ JAVASCRIPTS/evento.js
@@ -1,22 +1,21 @@
 function menus(){
     var opc= document.getElementById("OPC");
-    opc.innerHTML= `  <button id="OPC" onmouseenter="menus()" onmouseleave="menus2()"> <img src="Archivos/opcion-de-lista.png" width="30px" height="30px"> </button>
-    <table>
-        <tr>
-            <td> <a href="adopta.html"> <button><img src="Archivos/adopcion.png" width="50px" height="50px">ADOPTAR
-                    </button> </a> </td>
-            <td> <a href="formulario.html"> <button><img src="Archivos/desaparecido.png" width="50px"
-                            height="50px">PUBLICAR </button> </a> </td>
-            <td> <a href="perdidos.html"><button><img src="Archivos/desaparecido.png" width="50px" height="50px"> VER
-                    </button> </a> </td>
-        </tr>
+    opc.innerHTML= ` 
+    <div id="MENU3>         
+    <a href="adopta.html"> <button><img src="Archivos/adopcion.png" width="50px" height="50px">ADOPTAR
+                    </button> </a> 
+             <a href="formulario.html"> <button><img src="Archivos/desaparecido.png" width="50px"
+                            height="50px">PUBLICAR </button> </a> 
+             <a href="perdidos.html"><button><img src="Archivos/desaparecido.png" width="50px" height="50px"> VER
+                    </button> </a> 
+        <div>
 
-    </table>`
+    `
 }
 
 function menus2(){
     var opc= document.getElementById("OPC");
-    opc.innerHTML= ` <button id="OPC" onmouseenter="menus()" onmouseleave="menus2()"> <img src="Archivos/opcion-de-lista.png" width="30px" height="30px"> </button>
+    opc.innerHTML= ` <p id="OPC" onmouseenter="menus()" onmouseout="menus2()"> <img src="Archivos/opcion-de-lista.png" width="30px" height="30px"> </p>
     `
 }
 
